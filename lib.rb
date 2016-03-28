de72cca6a1e4baa0d51053aa51d35c641fc6a143
Zach Ahn <zach.ahn@gmail.com>
2016-03-27 22:56:31 -0400

    remove lib tests :see_no_evil:

--- lib.rb
+++ lib.rb
@@ -1,11 +1,3 @@
-if __FILE__ == $0
-  require "minitest/autorun"
-
-  $home = ENV["HOME"].dup.freeze
-  $pwd  = Dir.pwd.dup.freeze
-  $test = true
-end
-
 module Action
   def call
     if first_run?
@@ -27,41 +19,6 @@ class Sloth
 
   def dsm(name, &block)
     define_singleton_method(name, &block)
-  end
-end
-
-if $test
-  class ActionTest < Minitest::Test
-    def test_call_first
-      x = Object.new
-      x.singleton_class.include(Action)
-      x.define_singleton_method(:first_run?, -> { true })
-      x.define_singleton_method(:first, -> {})
-
-      x.call
-    end
-
-    def test_call_tail
-      x = Object.new
-      x.singleton_class.include(Action)
-      x.define_singleton_method(:first_run?, -> { false })
-      x.define_singleton_method(:tail, -> {})
-
-      x.call
-    end
-
-    def test_call_always
-      always_called = false
-      x = Object.new
-      x.singleton_class.include(Action)
-      x.define_singleton_method(:first_run?, -> { false })
-      x.define_singleton_method(:tail, -> {})
-      x.define_singleton_method(:always, -> { always_called = true})
-
-      x.call
-
-      assert_equal true, always_called
-    end
   end
 end
 
