895372d6c03a1d638edfedded9fab2e241b1f4ce
Zach Ahn <engineering@zachahn.com>
2018-05-07 17:42:20 -0400

    [zsh] Customize prompt

--- _zomg/setup/prompt.zsh
+++ _zomg/setup/prompt.zsh
@@ -1,14 +1,31 @@
-# Prompt
 autoload -Uz promptinit
 promptinit
 
-PROMPT="${SSH_TTY:+"%n@%m "}%~ %F{4}❯%f "
-PROMPT="${SSH_TTY:+"%F{1}%n@%m%f "}%~ %F{4}❯%f "
+setopt prompt_subst
 
+PROMPT='${SSH_TTY:+"%F{1}%n@%m%f "}%F{7}${PROMPT_PWD_DIRNAME}${PROMPT_PWD_SEPARATOR}%f${PROMPT_PWD_BASENAME} %F{4}❯%f '
 
 function precmd () {
   # Tab title
   print -Pn "\e]1;%m: %~\a"
   # Window title
   print -Pn "\e]2;%n@%m: %~\a"
+
+  local pwd_ish="${PWD/#$HOME/~}"
+  local pwd_dirname="$(dirname $pwd_ish)"
+  local pwd_basename="$(basename $pwd_ish)"
+
+  if [[ $pwd_dirname == $pwd_basename || $pwd_dirname == "." ]]; then
+    PROMPT_PWD_DIRNAME=""
+    PROMPT_PWD_SEPARATOR=""
+    PROMPT_PWD_BASENAME="${pwd_basename}"
+  elif [[ $pwd_dirname == "/" ]]; then
+    PROMPT_PWD_DIRNAME="/"
+    PROMPT_PWD_SEPARATOR=""
+    PROMPT_PWD_BASENAME="${pwd_basename}"
+  else
+    PROMPT_PWD_DIRNAME="${pwd_dirname}";
+    PROMPT_PWD_SEPARATOR="/"
+    PROMPT_PWD_BASENAME="${pwd_basename}"
+  fi
 }
