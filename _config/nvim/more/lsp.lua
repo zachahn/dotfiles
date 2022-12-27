be73eccda1e25edfef4cf2a5a4464b3f83d32d47
Zach Ahn <engineering@zachahn.com>
2022-12-26 20:57:42 -0500

    [nvim] Prevent text from jumping when there are LSP notices
    Going into insert mode removes the "signs" column, while going into
    normal mode shows the column.

--- _config/nvim/more/lsp.lua
+++ _config/nvim/more/lsp.lua
@@ -6,6 +6,12 @@ vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
 vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
 
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+  vim.lsp.diagnostic.on_publish_diagnostics, {
+    signs = false,
+  }
+)
+
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
 local on_attach = function(client, bufnr)
