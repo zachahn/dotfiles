8ee639f4f6e0891505bdd71a4997f70d06b1c380
Zach Ahn <engineering@zachahn.com>
2024-08-27 11:26:50 -0400

    [nvim] Treesitter for YAML

--- _config/nvim/lua/config/treesitter.lua
+++ _config/nvim/lua/config/treesitter.lua
@@ -1,4 +1,4 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the listed parsers MUST always be installed)
-  ensure_installed = { "ruby", "rust" },
+  ensure_installed = { "ruby", "rust", "yaml" },
 }
