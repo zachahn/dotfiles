cee8489c0f41d283aa85389b2049cc3650a82d7d
Zach Ahn <engineering@zachahn.com>
2024-08-28 14:48:44 -0400

    [nvim] Only show context names, not comments

--- _config/nvim/lua/config/treesitter.lua
+++ _config/nvim/lua/config/treesitter.lua
@@ -2,3 +2,7 @@ require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the listed parsers MUST always be installed)
   ensure_installed = { "ruby", "rust", "yaml" },
 }
+
+require'treesitter-context'.setup{
+  multiline_threshold = 1,
+}
